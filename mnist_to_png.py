f74c7572d6663cde4761b1acdf7b545a97df345c
hongikhs <sw@hongik.hs.kr>
2020-11-29 20:44:36 +0900

    Create mnist_to_png.py

---
+++ mnist_to_png.py
@@ -0,0 +1,20 @@
+import png
+import csv
+import numpy as np
+mnist_file = 'mnist_train_100'
+m = open(mnist_file + '.csv', encoding='utf-8')
+mnist = csv.reader(m, delimiter=',')
+fnum = 0
+
+for row in mnist:
+    label = row[0]
+    image = list(map(int, row[1:]))
+    print(label, len(image))
+    f = open(mnist_file + '\\' + label + '\\' + str(fnum) + '.png', 'wb')
+    w = png.Writer(28, 28, greyscale=True)
+    image2d = (255-np.array(image)).reshape((28, 28)).tolist()
+    w.write(f, image2d)
+    f.close()
+    fnum += 1
+
+m.close()
